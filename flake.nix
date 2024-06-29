b2e88d056d2d7a2c33033069a4685f1d3bdb7fca
Jacob Henn <windowWeasel@outlook.com>
2024-06-29 16:09:34 -0700

    add rust-analyzer to flake.lock

--- flake.nix
+++ flake.nix
@@ -19,11 +19,10 @@
       {
         devShells.default = mkShell {
           buildInputs = [
-            # openssl
             pkg-config
             (
               rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-                extensions = [ "rust-src" ];
+                extensions = [ "rust-src" "rust-analyzer" ];
                 targets = [ "x86_64-unknown-linux-gnu" ];
               })
             )
